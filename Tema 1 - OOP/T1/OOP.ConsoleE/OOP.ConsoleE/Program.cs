7c051ed11d60e51d318d8ae98a21125ce544200e
NathalieVHerreraP <99284820+NathalieVHerreraP@users.noreply.github.com>
2022-02-09 20:11:30 -0600

    tutoriales 1&2

---
+++ Tema 1 - OOP/T1/OOP.ConsoleE/OOP.ConsoleE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Lil World!");
+
+            Animal animal = new Animal();
+            animal.AnimalSound();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
