af8b6f8274a70917c794a75780fcaf3a6480bc22
NathalieVHerreraP <99284820+NathalieVHerreraP@users.noreply.github.com>
2022-03-03 13:04:01 -0600

    Tema 3

---
+++ Tema 3 - NetProjects/T6/WebApi.WindowsClient/WebApi.WindowsClient/Program.cs
@@ -0,0 +1,109 @@
+﻿using Api.Entities;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.WindowsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetIp();
+
+            System.Threading.Thread.Sleep(5000);
+
+            var forecast = GetWeatherForecastAsync(new Zone
+            {
+                City = "Acapulco",
+                Date = new DateTime(2021, 10, 20)
+            }).Result;
+
+            Console.WriteLine("ciudad: " + forecast.Zone.City);
+            Console.WriteLine("Date: " + forecast.Zone.Date);
+
+            foreach (var item in forecast.weatherForecast)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Summary: " + item.Summary);
+                Console.WriteLine("TemperatureC: " + item.TemperatureC);
+                Console.WriteLine("TemperatureF: " + item.TemperatureF);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task<ZoneWeatherForecast> GetWeatherForecastAsync(Zone zone) 
+        {
+
+            string jsonContent = Newtonsoft.Json.JsonConvert.SerializeObject(zone);
+
+            HttpClient client = new HttpClient();
+
+
+            HttpResponseMessage response = await client.PostAsync("https://localhost:44314/weatherforecast/byzone", new StringContent(jsonContent, Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            ZoneWeatherForecast forecast = Newtonsoft.Json.JsonConvert.DeserializeObject<ZoneWeatherForecast>(responseBody);
+
+           // Console.WriteLine("My current Ip Address is: " + ip.Ip);
+
+            //var info = await GetIpInfo(ip.Ip);
+
+            //Console.WriteLine("Country is: " + info.Country);
+            //Console.WriteLine("City is: " + info.City);
+            //Console.WriteLine("TimeZone is: " + info.TimeZone);
+
+            return forecast;
+        }
+
+        private static async  Task<IpAddress> GetIp()
+        {
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync("https://api.ipify.org/?format=json");
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            IpAddress ip = Newtonsoft.Json.JsonConvert.DeserializeObject<IpAddress>(responseBody);
+
+            Console.WriteLine("My current Ip Address is: " + ip.Ip);
+
+            var info = await GetIpInfo(ip.Ip);
+
+            Console.WriteLine("Country is: " + info.Country);
+            Console.WriteLine("City is: " + info.City);
+            Console.WriteLine("TimeZone is: " + info.TimeZone);
+
+            return ip;
+
+        }
+
+        private static async Task<IpInfo> GetIpInfo(string ipAddress)
+        {
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync($"https://ipinfo.io/{ipAddress}/geo");
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            IpInfo info = Newtonsoft.Json.JsonConvert.DeserializeObject<IpInfo>(responseBody);
+
+            
+
+            return info;
+
+        }
+
+    }
+}
