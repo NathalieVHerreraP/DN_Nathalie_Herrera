aff69668b4b0a98aa664074bd01afd68e1b7f31b
NathalieVHerreraP <99284820+NathalieVHerreraP@users.noreply.github.com>
2022-04-05 20:24:00 -0500

    Tema 6
    Tutorial 8 completed

---
+++ Tema 6 - AspNetCore MVC con Entity Framework/T8/GymManager.Web/GymManager.ApplicationServices/Products/iEquipmentTypesAppService.cs
@@ -0,0 +1,22 @@
+﻿using GymManager.Core.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.ApplicationServices.Products
+{
+    public interface iEquipmentTypesAppService
+    {
+        Task<List<EquipmentType>> GetEquipmentsAsync();
+
+        Task<int> AddEquipmentAsync(EquipmentType equipment);
+
+        Task DeleteEquipmentAsync(int memberId);
+
+        Task<EquipmentType> GetEquipmentAsync(int equipmentId);
+
+        Task EditEquipmentAsync(EquipmentType equipmentId);
+    }
+}
---
+++ Tema 6 - AspNetCore MVC con Entity Framework/T8/GymManager.Web/GymManager.Web/Models/MembershipTypesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GymManager.Core.Members;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Models
+{
+    public class MembershipTypesViewModel
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+
+        public decimal Cost { get; set; }
+
+
+        public int MonthsDuration { get; set; }
+    }
+}
