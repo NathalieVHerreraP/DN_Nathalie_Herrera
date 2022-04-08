530b1034f6977ab212085a17cc02a3b49ab366d8
NathalieVHerreraP <99284820+NathalieVHerreraP@users.noreply.github.com>
2022-04-07 20:28:06 -0500

    Tema 6
    Tutorial no. 9 finished

---
+++ Tema 6 - AspNetCore MVC con Entity Framework/T9/GymManager.Web/GymManager.Web/Models/MemberViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GymManager.Core.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Models
+{
+    public class MemberViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+   
+        public string LastName { get; set; }
+
+     
+        public DateTime Membersince { get; set; }
+
+        
+        public DateTime Birthday { get; set; }
+
+        
+        public int CityId { get; set; }
+
+       
+        public string Email { get; set; }
+
+        public bool AllowNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+    }
+}
---
+++ Tema 6 - AspNetCore MVC con Entity Framework/T9/GymManager.Web/GymManager.Web/Views/Shared/Components/AppMenu/AppMenuViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using GymManager.ApplicationServices.Navegation;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Views.Shared.Components.AppMenu
+{
+    public class AppMenuViewComponent : ViewComponent
+    {
+
+        private readonly iMenuAppService _menuAppService;
+        public AppMenuViewComponent(iMenuAppService menuAppService) 
+        {
+
+            _menuAppService = menuAppService;
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(String currentPageName = null)
+        {
+            var model = new MenuViewModel
+            {
+                CurrentPageName = currentPageName,
+                Menu = _menuAppService.GetMenu()
+            };
+
+            return View("Default",model);
+        }
+    }
+
+    
+}
